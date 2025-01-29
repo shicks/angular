d9090b3d2be564312e6f0d51dc1733b98b723eed
Stephen Hicks <shicks@users.noreply.github.com>
2025-01-29 18:33:23 -0500

    feat(common): throw error for suspicious date patterns
    Adjusts the date pipe and formatDate function to detect suspicious usages of the week-numbering year formatter without including the week number, as this is often confused for the calendar year and likely to result in incorrect results near New Years, meaning that those issues aren't typically caught during development. This commit starts throwing a development-only error to reveal this issue right away.
    
    BREAKING CHANGE:
    
    Using the `Y` formatter (week-numbering year) without also including `w` (week number) is now detected as suspicious date pattern, as `y` is typically intended.

--- packages/common/src/i18n/format_date.ts
+++ packages/common/src/i18n/format_date.ts
@@ -7,6 +7,11 @@
  */
 
 import {
+  ɵRuntimeError as RuntimeError,
+  ɵformatRuntimeError as formatRuntimeError,
+} from '@angular/core';
+
+import {
   FormatWidth,
   FormStyle,
   getLocaleDateFormat,
@@ -24,6 +29,7 @@ import {
   Time,
   TranslationWidth,
 } from './locale_data_api';
+import {RuntimeErrorCode} from '../errors';
 
 export const ISO8601_DATE_REGEX =
   /^(\d{4,})-?(\d\d)-?(\d\d)(?:T(\d\d)(?::?(\d\d)(?::?(\d\d)(?:\.(\d+))?)?)?(Z|([+-])(\d\d):?(\d\d))?)?$/;
@@ -83,6 +89,10 @@ export function formatDate(
   locale: string,
   timezone?: string,
 ): string {
+  if (typeof ngDevMode === 'undefined' || ngDevMode) {
+    assertValidDateFormat(format);
+  }
+
   let date = toDate(value);
   const namedFormat = getNamedFormat(locale, format);
   format = namedFormat || format;
@@ -121,6 +131,29 @@ export function formatDate(
   });
 
   return text;
+}
+
+/**
+ * Asserts that the given date format is free from common mistakes.  Throws an
+ * error if one is found (except for the case of all "Y", in which case we just
+ * log a warning).  This should only be called in development mode.
+ */
+function assertValidDateFormat(format: string) {
+  if (format.includes('Y') && !format.includes('w')) {
+    // "Y" indicates "week-based year", which differs from the actual calendar
+    // year for a few days around Jan 1 most years.  Unless "w" is also
+    // present (e.g. a date like "2024-W52") this is likely a mistake.  Users
+    // probably meant "y" instead.
+    const message =
+        `Suspicious use of week-based year "Y" in date pattern "${format}"\n` +
+            `Did you mean to use calendar year "y" instead?`;
+    if (/^Y+$/.test(format)) {
+      // NOTE: allow "YYYY" with just a warning, since it's used in tests.
+      console.error(formatRuntimeError(RuntimeErrorCode.SUSPICIOUS_DATE_FORMAT, message));
+    } else {
+      throw new RuntimeError(RuntimeErrorCode.SUSPICIOUS_DATE_FORMAT, message);
+    }
+  }
 }
 
 /**
